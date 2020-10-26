d7c9292bda85973bc279deb017687d4d7b947a28
Sandrine <sandrine.godmez@gmail.com>
2020-10-26 16:29:43 +0100

    Full Counter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import HelloJsx from "./HelloJsx";
+import Counter from "./Counter";
 
 export default function App() {
     return (
-        <HelloJsx/>
+       [<HelloJsx/>, <Counter/>]
     );
 }
